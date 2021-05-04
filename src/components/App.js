939953ba417603ee8d93c2c256c63bd2cc70059b
samer senbol <samer.senbol@gmail.com>
2021-05-04 15:07:16 +0200

    Create basic files
    - Create basic required components
    
    Fix #1

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Team from './Team'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Team />
+    </div>
+  )
+}
+
+export default App
