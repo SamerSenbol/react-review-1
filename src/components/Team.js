c8ccf42502167642145b33029f850114a655ec21
samer senbol <samer.senbol@gmail.com>
2021-05-06 19:26:50 +0200

    Add condition
    - Use if condition to distinguish the members who don't have website
    - Use map function to retrieve the data from an array
    
    fix #3

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,80 +2,32 @@ import React from 'react'
 import TeamMember from './TeamMember'
 
 function Team (){
+    
+    const info=[
+        {id:1, img:'./images/01.jpg',    name:'Walter White',    position:'Bryan Cranston',   phone:'+49 12345678',   email:'walter@gmail.com',   website:'walterwhite.com'},
+        {id:2, img:'./images/02.jpg',    name:'Jesse Pinkman',   position:'Bryan Cranston',   phone:'+49 12345678',   email:'walter@gmail.com',   website:'walterwhite.com'},
+        {id:3, img:'./images/03.jpg',     name:'Skyler white',    position:'Bryan Cranston',  phone:'+49 12345678',   email:'walter@gmail.com',   website:'walterwhite.com'},
+        {id:4, img:'./images/04.jpg',     name:'Gus Fring',       position:'Bryan Cranston',  phone:'+49 12345678',    email:'walter@gmail.com'},
+        {id:5, img:'./images/05.jpg',     name:'Saul Godman',     position:'Bryan Cranston',  phone:'+49 12345678',   email:'walter@gmail.com'},
+        {id:6, img:'./images/06.jpg',     name:'Hank Schrader ',  position:'Bryan Cranston',  phone:'+49 12345678',   email:'walter@gmail.com'}
+    ]
+
+    const members= info.map(member =>
+        <TeamMember
+            key={member.id} 
+            img={member.img}
+            name={member.name}
+            position={member.position}
+            phone={member.phone}
+            email={member.email}
+            website={member.website}
+        />
+    );
+
+
     return(
         <div className='row'>
-            <TeamMember 
-            info={
-                {
-                    img:'./images/01.jpg',
-                    name:'Walter White',
-                    position:'Bryan Cranston',
-                    phone:'+49 12345678',
-                    email:'walter@gmail.com',
-                    website:'walterwhite.com'
-                }
-            }
-            />
-            <TeamMember 
-            info={
-                {
-                    img:'./images/02.jpg',
-                    name:'Jesse Pinkman',
-                    position:'Bryan Cranston',
-                    phone:'+49 12345678',
-                    email:'walter@gmail.com',
-                    website:'walterwhite.com'
-                }
-            }
-            />
-            <TeamMember 
-            info={
-                {
-                    img:'./images/03.jpg',
-                    name:'Skyler white',
-                    position:'Bryan Cranston',
-                    phone:'+49 12345678',
-                    email:'walter@gmail.com',
-                    website:'walterwhite.com'
-                }
-            }
-            />
-            <TeamMember 
-            info={
-                {
-                    img:'./images/04.jpg',
-                    name:'Gus Fring',
-                    position:'Bryan Cranston',
-                    phone:'+49 12345678',
-                    email:'walter@gmail.com',
-                    website:'walterwhite.com'
-                }
-            }
-            />
-            <TeamMember 
-            info={
-                {
-                    img:'./images/05.jpg',
-                    name:'Saul Godman',
-                    position:'Bryan Cranston',
-                    phone:'+49 12345678',
-                    email:'walter@gmail.com',
-                    website:'walterwhite.com'
-                }
-            }
-            />
-            <TeamMember 
-            info={
-                {
-                    img:'./images/06.jpg',
-                    name:'Hank Schrader ',
-                    position:'Bryan Cranston',
-                    phone:'+49 12345678',
-                    email:'walter@gmail.com',
-                    website:'walterwhite.com'
-                }
-            }
-            />
+            {members}
         </div>
     )
 }
--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -5,15 +5,14 @@ function TeamMember (props){
         <div className='col-md-4 col-sm-6'>
             <div className='card'>
                 <div className='card-header'>
-                    <img style={{maxWidth:'100%'}}src={props.info.img}/>
+                    <img style={{maxWidth:'100%'}} src={props.img} alt=""/>
                 </div>
-                <div className='card-body'>
-                    <h2>{props.info.name}</h2>
-                    <h5>{props.info.position}</h5>
-                    <h5>{props.info.phone}</h5>
-                    <h5>{props.info.email}</h5>
-                    <h5>{props.info.website}</h5>
-
+                <div className='card-body'style={{backgroundColor: props.website ? 'white': 'black'}} >
+                    <h2>{props.name}</h2>
+                    <h5>{props.position}</h5>
+                    <h5>{props.phone}</h5>
+                    <h5>{props.email}</h5>
+                    <h5>{props.website}</h5>
                 </div>
             </div>
         </div>
